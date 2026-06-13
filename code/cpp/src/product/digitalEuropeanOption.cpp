6c38b20b8bc1b67e186b4f6849ad2b481338f09b
EspejoLuis <espejoluis89@gmail.com>
2026-06-13 21:01:30 +0100

    feat: digital european option in MC

---
+++ code/cpp/src/product/digitalEuropeanOption.cpp
@@ -0,0 +1,49 @@
+#include "digitalEuropeanOption.h"
+
+DigitalEuropeanOption::DigitalEuropeanOption(
+    const DigitalOptionParameters& optParams)
+    : optParams_(optParams) {
+
+    validateInputs();
+};
+
+double DigitalEuropeanOption::payoff(const std::vector<double>& path) const {
+
+    const double sign =
+        optParams_.direction == OptionDirection::Long ? 1.0 : -1.0;
+
+    const double underlyingPrice = path.back();
+
+    const bool indicator = optParams_.type == OptionType::Call
+                               ? underlyingPrice > optParams_.strike
+                               : underlyingPrice < optParams_.strike;
+
+    const double payoutAmount =
+        optParams_.digitalType == DigitalType::AssetOrNothing
+            ? underlyingPrice
+            : optParams_.strike;
+
+    return sign * indicator * payoutAmount;
+};
+
+const DigitalOptionParameters& DigitalEuropeanOption::parameters() const {
+    return optParams_;
+};
+
+void DigitalEuropeanOption::validateInputs() const {
+
+    if (optParams_.strike < 0)
+        throw std::invalid_argument("strike cannot be negative");
+
+    if (optParams_.type != OptionType::Call &&
+        optParams_.type != OptionType::Put)
+        throw std::invalid_argument("invalid OptionType");
+
+    if (optParams_.direction != OptionDirection::Short &&
+        optParams_.direction != OptionDirection::Long)
+        throw std::invalid_argument("invalid OptionDirection");
+
+    if (optParams_.digitalType != DigitalType::AssetOrNothing &&
+        optParams_.digitalType != DigitalType::CashOrNothing)
+        throw std::invalid_argument("invalid DigitalType");
+};
---
+++ code/cpp/src/product/digitalEuropeanOption.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "instrument.h"
+#include "optionParameters.h"
+
+enum class DigitalType { CashOrNothing, AssetOrNothing };
+
+struct DigitalOptionParameters {
+    double strike;
+    OptionType type;
+    OptionDirection direction;
+    DigitalType digitalType;
+};
+
+class DigitalEuropeanOption : public Instrument {
+
+  public:
+    DigitalEuropeanOption(const DigitalOptionParameters& optParams);
+
+    double payoff(const std::vector<double>& path) const override;
+
+    const DigitalOptionParameters& parameters() const;
+
+  private:
+    const DigitalOptionParameters optParams_;
+
+    void validateInputs() const;
+};
